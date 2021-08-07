c745d890bc00e788d862106367f5ea5a1fae75ee
straightmin <straightmin@gmail.com>
2021-08-07 15:12:25 +0900

    Create 11726.cpp

---
+++ 11726_2xn 타일링.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int DP[n+1] = {};
+    DP[1] = 1, DP[2] = 2;
+    
+    for(int i = 3; i <= n; i++){
+        DP[i] = (DP[i-1] + DP[i-2]) % 10007;
+    }
+    
+    cout << DP[n];
+    
+    return 0;
+}
+
